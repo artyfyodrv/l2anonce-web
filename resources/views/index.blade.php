ddd1d8e92fd73bb4136d0f4f365bd244ad775525
afedorov <afedorov@team.amocrm.com>
2023-09-29 01:47:16 +0300

    #3 html+css через grid разметку, роут + контроллер

---
+++ resources/views/index.blade.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/index.css" media="screen" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Hubballi&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>L2Rates - рейтинг серверов Lineage II</title>
+</head>
+<body>
+<div class="grid">
+    <header>
+        <div class="server_banner">
+            <img src="../img/banner.png">
+        </div>
+    </header>
+    <nav>
+        <div class="left-buttons">
+            <a class="button-nav b-contacts" href=""><span class="button-text contact-text">Контакты</span></a>
+            <a class="button-nav b-services" href=""><span class="button-text">Услуги</span></a>
+        </div>
+        <div class="n-logo">
+            <img src="../img/logo.png">
+            <a class="logo">L2anoncer-web</a>
+        </div>
+        <div class="right-buttons">
+            <a class="button-nav b-streamers" href=""><span class="button-text streamers-text">Стримерам</span></a>
+            <a class="button-nav b-servers" href=""><span class="button-text">Разместить</span></a>
+        </div>
+    </nav>
+    <content>
+        <div class="wrapper">
+            <div class="streamers-container">
+                <a class='name_category'><span class="name_body">Стримы по Lineage II &#128101;</span></a>
+                <div class="streamer streamer-one">
+                    <a class="free_slot" href="">Свободное место 1</a>
+                </div>
+                <div class="streamer streamer-two">
+                    <a class="free_slot" href="">Свободное место 2</a>
+                </div>
+                <div class="streamer streamer-three">
+                    <a class="free_slot" href="">Свободное место 3</a>
+                </div>
+                <span class="close_container"></span>
+            </div>
+
+            {{--КОНТЕЙНЕРЫ С СЕРВЕРАМИ--}}
+            <div class="servers-container">
+                <a class='name_category'><span class="name_body">Анонсы новых серверов Lineage II</span></a>
+                <div class="left-containers">
+                    <a class='page soon_open'>Скоро открываются</a>
+                    <div class="soon_open_servers">
+                            <ul class="get_server">
+                                <li class="server status_server" href="">TOP</li>
+                                <li class="server name_server" href=""></li>
+                                <li class="server icons_server" href=""></li>
+                                <li class="server rates_server" href=""></li>
+                                <li class="server chronicle_server" href=""></li>
+                                <li class="server date_server"></li>
+                            </ul>
+                    </div>
+                    <div class="yesterday_servers">
+                        <a class='page today_open'>Открываются сегодня</a>
+                        <div class="soon_open_servers">
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server" href=""></li>
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="tomorrow_servers">
+                        <a class='page today_open'>Открываются завтра</a>
+                        <div class="soon_open_servers">
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server" href=""></li>
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="opens_7day_servers">
+                        <a class='page today_open'>Ближайшую неделю</a>
+                        <div class="soon_open_servers">
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server" href=""></li>
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="tomorrow_servers">
+                        <a class='page today_open'>Более недели</a>
+                        <div class="soon_open_servers">
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server" href=""></li>
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-containers">
+                    <div class="opened_servers">
+                        <a class='page today_open'>Открытые сервера</a>
+                        <div class="soon_open_servers">
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server" href=""></li>
+                                </ul>
+                        </div>
+                        <div class="last_7day_servers">
+                            <a class='page today_open'>Открылись вчера</a>
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server"
+                                        href=""></li>
+                                </ul>
+                        </div>
+                        <div class="last_7day_servers">
+                            <a class='page today_open'>Предыдущую неделю</a>
+                                <ul class="get_server">
+                                    <li class="server status_server" href="">TOP</li>
+                                    <li class="server name_server" href=""></li>
+                                    <li class="server icons_server" href=""></li>
+                                    <li class="server rates_server" href=""></li>
+                                    <li class="server chronicle_server" href=""></li>
+                                    <li class="server date_server"
+                                        href=""></li>
+                                </ul>
+                        </div>
+                        <div class="opened_7plus_servers">
+                            <a class='page today_open'>Более недели назад</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="filters-container">
+                <a class='filter__names'><span class="name_body">Фильтры по серверам &#128269;</span></a>
+                <div class="rates_buttons">
+                    <a class="filter filter_chronicle">High Five</a>
+                    <a class="filter filter_chronicle">Essence</a>
+                    <a class="filter filter_chronicle">Classic</a>
+                    <a class="filter filter_chronicle">C4</a>
+                    <a class="filter filter_chronicle">Interlude</a>
+                    <a class="filter filter_chronicle">Interlude+</a>
+                    <a class="filter filter_chronicle">Epilogue</a>
+                    <a class="filter filter_chronicle">Final</a><br>
+
+                    <div class="button_chronicles">
+                        <a class="button_all_chronicles">Все хроники</a>
+                    </div>
+
+                </div>
+                <span class="close_container"></span>
+            </div>
+            <div class="promo-container">
+                <a class='filter__names'><span class="name_body">Рекомендуем</span></a>
+                <img src="../img/minibanner.jpg">
+                <span class="close_container"></span>
+            </div>
+            <div class="rates-container">
+                <a class='filter__names'><span class="name_body">Фильтры по рейтам&#128269;</span></a>
+                <div class="rates_buttons">
+                    <a class="filter filter_rates">x1</a>
+                    <a class="filter filter_rates">x3</a>
+                    <a class="filter filter_rates">x5</a>
+                    <a class="filter filter_rates">x10</a>
+                    <a class="filter filter_rates">x25</a>
+                    <a class="filter filter_rates">x50</a>
+                    <a class="filter filter_rates">x100</a>
+                    <a class="filter filter_rates">x100-x1200</a>
+                    <a class="filter filter_rates">x1200+</a>
+                </div>
+                <span class="close_container"></span>
+            </div>
+        </div>
+    </content>
+</div>
+</body>
+</html>
