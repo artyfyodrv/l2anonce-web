0cca2b2cac7d13e4a1d3dd6e28292b706b279e9e
afedorov <afedorov@team.amocrm.com>
2023-10-02 22:17:48 +0300

    #5 выборка с бд серверов по status и  is_visible, вывод на главную по блокам

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\IndexServices;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function show()
     {
-        return view('index');
+        $serversList = IndexServices::getServersList();
+
+        return view('index', compact('serversList'));
     }
 }
